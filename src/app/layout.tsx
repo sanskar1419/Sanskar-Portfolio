c55cbc52d9faabb78e9977a8785de2ddb111e263
Sanskar Gupta <sanskar.gupta@uponly.in>
2024-12-15 22:24:28 +0530

    Complete header section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,22 @@
 import type { Metadata } from "next";
+import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
+import { twMerge } from "tailwind-merge";
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Sanskar | Portfolio",
+  description:
+    "A creative problem solver with a passion for building fast, scalable, and user-friendly applications. I have experience with a wide range of technologies, including React, Next.js, Node.js, Express, and MongoDB. I'm always looking for new ways to improve my skills and create innovative solutions.",
 };
+
+// This line imports the Inter font with the latin subset and assigns it to the "--font-sans" CSS variable.
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+// This line imports the Calistoga font with the latin subset and assigns it to the "--font-serif" CSS variable with a weight of 400.
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +25,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans"
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
